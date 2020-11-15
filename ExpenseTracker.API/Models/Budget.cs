638cc7512926713f8d11ca7c2fb561e9580fcf9a
Lukas Stolt <lukasstolt@gmail.com>
2020-11-15 17:52:09 +0100

    Added basic models

---
+++ ExpenseTracker.API/Models/Budget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.API.Models
+{
+	public class Budget
+	{
+		public int Id { get; set; }
+		public DateTime Month { get; set; }
+		public List<BudgetItem> Expenses { get; set; }
+		public List<BudgetItem> Income { get; set; }
+
+		public decimal AmountLeft()
+		{
+			return Income.Sum(i => i.Amount) - Expenses.Sum(e => e.Amount);
+		}
+	}
+}
---
+++ ExpenseTracker.API/Models/BudgetItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.API.Models
+{
+	public class BudgetItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Amount { get; set; }
+		public DateTime DateTime { get; set; }
+		public Category Category { get; set; }
+	}
+}
