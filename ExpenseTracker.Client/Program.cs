a4860a18137118e09c51b7784e5ccdee071fa0ac
Lukas Stolt <lukasstolt@gmail.com>
2020-11-21 18:09:06 +0100

    Added MatBlazor Added two MatTables with the budget data on the budget page Created an about page

--- ExpenseTracker.Client/Program.cs
+++ ExpenseTracker.Client/Program.cs
@@ -1,3 +1,4 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace ExpenseTracker.Client
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddMatBlazor();
 
 			await builder.Build().RunAsync();
 		}
