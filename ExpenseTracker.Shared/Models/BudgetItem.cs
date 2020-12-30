f6c4f3034304bbe26de7cb71c0c0e5c59e7cf14f
Lukas Stolt <lukasstolt@gmail.com>
2020-12-30 18:00:28 +0100

    Changed budgetitem field name

--- ExpenseTracker.Shared/Models/BudgetItem.cs
+++ ExpenseTracker.Shared/Models/BudgetItem.cs
@@ -13,7 +13,7 @@ namespace ExpenseTracker.Shared.Models
 		[Key]
 		public int Id { get; set; }
 		[Required]
-		public string Name { get; set; }
+		public string Description { get; set; }
 		[Required]
 		public decimal Amount { get; set; }
 		[Required]
